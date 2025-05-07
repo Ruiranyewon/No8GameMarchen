7409460cd9819535f731a31ce060ef3932da66eb
Ruiranyewon <ruiran501@hanyang.ac.kr>
2025-05-07 19:32:26 +0900

    CharacterMovementAndLoadScene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public float runSpeed;
+    public int walkCount;
+    private Vector3 vector;
+    private float applyRunSpeed;
+    private int currentWalkCount;
+    private bool canMove = true;
+    void Start()
+    {
+        IEnumerator MoveCoroutine()
+        {
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                applyRunSpeed = runSpeed;
+            } else
+                applyRunSpeed = 0;
+
+            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
+            if(vector.x != 0)
+            {
+                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
+            } else if(vector.y != 0)
+            {
+                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
+            }
+
+            currentWalkCount++;
+        }
+        currentWalkCount = 0;
+    }
+}
---
+++ Assets/Scripts/SceneSwitch.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public void LoadScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void LoadScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+
+    public void LoadScene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+
+    public void LoadScene4()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+}
