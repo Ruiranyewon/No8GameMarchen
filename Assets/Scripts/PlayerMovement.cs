aaae1c51497f054a758f949d69fbc9dde83ef93e
YunDain <144747386+Yundaim@users.noreply.github.com>
2025-05-16 20:14:51 +0900

    Character Animation Script

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,46 +1,61 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
-    public float runSpeed;
-    public int walkCount;
-    private Vector3 vector;
-    private float applyRunSpeed;
-    private int currentWalkCount;
-    private bool canMove = true;
-    void Start()
+    public float speed = 3f;
+    public float runSpeed = 2f;
+
+    private Vector2 moveInput;
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    private void Start()
     {
-        IEnumerator MoveCoroutine()
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        // --- ① 입력 처리 ---
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // 대각선 이동 보정
+        if (moveInput.sqrMagnitude > 1)
+            moveInput = moveInput.normalized;
+
+        // 쉬프트로 달리기
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? speed + runSpeed : speed;
+
+        // --- ② 애니메이터에 값 전달 ---
+        animator.SetFloat("moveX", moveInput.x);
+        animator.SetFloat("moveY", moveInput.y);
+
+        if (moveInput != Vector2.zero)
         {
-            yield return new WaitForSeconds(1f);
+            animator.SetBool("isMoving", true);
+
+            // 방향 결정: 하나의 축만 저장
+            if (Mathf.Abs(moveInput.x) > Mathf.Abs(moveInput.y))
+            {
+                animator.SetFloat("lastmoveX", Mathf.Sign(moveInput.x));
+                animator.SetFloat("lastmoveY", 0);
+            }
+            else
+            {
+                animator.SetFloat("lastmoveX", 0);
+                animator.SetFloat("lastmoveY", Mathf.Sign(moveInput.y));
+            }
+        }
+        else
+        {
+            animator.SetBool("isMoving", false);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
-        {
-            if(Input.GetKey(KeyCode.LeftShift))
-            {
-                applyRunSpeed = runSpeed;
-            } else
-                applyRunSpeed = 0;
-
-            vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), transform.position.z);
-            if(vector.x != 0)
-            {
-                transform.Translate(vector.x * (speed + applyRunSpeed), 0, 0);
-            } else if(vector.y != 0)
-            {
-                transform.Translate(0, vector.y * (speed + applyRunSpeed), 0);
-            }
-
-            currentWalkCount++;
-        }
-        currentWalkCount = 0;
+        float currentSpeed = Input.GetKey(KeyCode.LeftShift) ? speed + runSpeed : speed;
+        rb.MovePosition(rb.position + moveInput * currentSpeed * Time.fixedDeltaTime);
     }
 }
