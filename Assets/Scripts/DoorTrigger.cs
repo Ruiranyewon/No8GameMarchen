594f5ac5947a3f1954ce41e86695e94bf9c60175
Ruiranyewon <ruiran501@hanyang.ac.kr>
2025-06-04 09:42:48 +0900

    keyDoorInteraction

--- Assets/Scripts/DoorTrigger.cs
+++ Assets/Scripts/DoorTrigger.cs
@@ -1,14 +1,26 @@
+using System.Collections;
 using UnityEngine;
 using TMPro;
 
 public class DoorTrigger : MonoBehaviour
 {
     public bool hasKey = false; // Set true externally when the player obtains a key
+    public static bool isDialoguePlaying = false;
     public GameObject messagePanel;
-    public TMP_Text messageText;
+    public TMP_Text dialogueText;
+    public TMP_Text nameText;
+    public DialogueLine[] linesWithKey;
+    public DialogueLine[] linesWithoutKey;
+    public float typingSpeed = 0.05f;
+    private int index = 0;
+    private DialogueLine[] linesToUse;
     public SceneSwitch sceneSwitch;
 
     private bool playerInRange = false;
+
+    private bool isTyping = false;
+    private PlayerMovement playerMovement;
+    public SpeakerProfile noneSpeakerProfile;
 
     void Start()
     {
@@ -16,24 +28,51 @@ public class DoorTrigger : MonoBehaviour
         {
             messagePanel.SetActive(false);
         }
+
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerMovement = player.GetComponent<PlayerMovement>();
+        }
     }
 
     void Update()
     {
-        if (playerInRange && Input.GetKeyDown(KeyCode.F))
+        // F키로 대화 시작
+        if (playerInRange && !messagePanel.activeSelf && Input.GetKeyDown(KeyCode.F))
         {
-            if (hasKey)
+            StartCoroutine(StartDialogue(hasKey));
+        }
+
+        // Enter키로 대사 진행
+        if (messagePanel.activeSelf && Input.GetKeyDown(KeyCode.Return))
+        {
+            if (isTyping)
             {
-                Debug.Log("문 열기 가능 : 씬 전환 가능");
-                messageText.text = "You opened the door.";
-                messagePanel.SetActive(true);
-                Invoke("LoadNextScene", 0.5f); // Add delay before scene load
+                StopAllCoroutines();
+                dialogueText.text = linesToUse[index].sentence;
+                isTyping = false;
             }
             else
             {
-                messageText.text = "The door is locked. You need a key.";
-                messagePanel.SetActive(true);
-                Invoke("HideMessage", 2f);
+                index++;
+                if (index < linesToUse.Length)
+                {
+                    StartCoroutine(TypeSentence());
+                }
+                else
+                {
+                    messagePanel.SetActive(false);
+                    dialogueText.text = "";
+                    nameText.text = "";
+                    if (playerMovement != null)
+                        playerMovement.enabled = true;
+
+                    if (hasKey)
+                    {
+                        Invoke("LoadNextScene", 0.5f);
+                    }
+                }
             }
         }
     }
@@ -44,11 +83,6 @@ public class DoorTrigger : MonoBehaviour
         {
             sceneSwitch.LoadScene2();
         }
-    }
-
-    void HideMessage()
-    {
-        messagePanel.SetActive(false);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -67,5 +101,43 @@ public class DoorTrigger : MonoBehaviour
             if (messagePanel != null)
                 messagePanel.SetActive(false);
         }
+    }
+
+    IEnumerator StartDialogue(bool keyAvailable)
+    {
+        yield return new WaitForSeconds(0.5f);
+        if (messagePanel != null)
+            messagePanel.SetActive(true);
+
+        if (playerMovement != null)
+            playerMovement.enabled = false;
+
+        DialogueLine[] currentLines = keyAvailable ? linesWithKey : linesWithoutKey;
+        index = 0;
+
+        if (currentLines.Length > 0)
+        {
+            linesToUse = currentLines;
+            StartCoroutine(TypeSentence());
+        }
+    }
+
+    IEnumerator TypeSentence()
+    {
+        isTyping = true;
+
+        SpeakerProfile speaker = linesToUse[index].speaker != null ? linesToUse[index].speaker : noneSpeakerProfile;
+
+        nameText.text = speaker.speakerName;
+        nameText.color = speaker.speechColor;
+
+        dialogueText.text = "";
+        foreach (char letter in linesToUse[index].sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
     }
 }
