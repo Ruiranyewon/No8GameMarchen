233c6c4c855e7b1e8f1255fb59dadc6fb1b5506c
YunDain <144747386+Yundaim@users.noreply.github.com>
2025-06-05 03:00:46 +0900

    3

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class Node
 {
+    public bool isWall;
     public bool walkable;
     public Vector3 worldPosition;
+    public int x, y;
     public int gridX;
     public int gridY;
 
@@ -11,13 +13,23 @@ public class Node
     public int hCost;
     public Node parent;
 
-    public int fCost { get { return gCost + hCost; } }
+    public int fCost
+    {
+        get { return gCost + hCost; }
+    }
 
-    public Node(bool _walkable, Vector3 _worldPos, int _x, int _y)
+    public Node(bool _walkable, Vector3 _worldPosition, int _gridX, int _gridY)
     {
         walkable = _walkable;
-        worldPosition = _worldPos;
-        gridX = _x;
-        gridY = _y;
+        worldPosition = _worldPosition;
+        gridX = _gridX;
+        gridY = _gridY;
+    }
+
+    public Node(bool isWall, int x, int y)
+    {
+        this.isWall = isWall;
+        this.x = x;
+        this.y = y;
     }
 }
